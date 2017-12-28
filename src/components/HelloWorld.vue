4cc5f58c4cdff9f3213af6de6e5e0d943f6c16cb
Aaron <511451967@qq.com>
2017-12-28 23:03:59 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-button @click="onAjax">按钮</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import http from '@/libs/http'
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        visible: false
+      }
+    },
+    methods: {
+      onAjax: function () {
+        this.apiGet('index.php?a=index&m=Test&g=Api', {token: '123'}).then((res) => {
+          console.log(res)
+        })
+      }
+    },
+    mixins: [http]
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
